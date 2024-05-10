c58c5e66ef5ab006109244937dd24c50db6fca46
egalbornoz <egalbornoz@gmail.com>
2024-05-10 13:35:30 -0400

    Middlewares

---
+++ Autolavado/Middleware/MiddlewareException.cs
@@ -0,0 +1,16 @@
+using System.Net;
+
+namespace Autolavado.Middleware;
+
+
+public class MiddlewareException : Exception
+{
+    public HttpStatusCode Codigo { get; set; }
+    public object? Errores { get; set; }
+    public MiddlewareException(HttpStatusCode codigo, object? errores = null)
+    {
+        Codigo = codigo;
+        Errores = errores;
+    }
+
+}
